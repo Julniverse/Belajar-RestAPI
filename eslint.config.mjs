046bfd24e4440d9f0f1f1ce76c4d740a7908c934
Julniverse <juliandikurniansyah@gmail.com>
2025-01-26 22:03:45 +0700

    Day 5

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,31 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}, **/*.ts", "**/*.tsx", "**/*.js"]},
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.node }},
+  {
+    files: ["**/*.{js,mjs,cjs,ts,tsx}"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+      globals: globals.node,
+    },
+  },
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      sourceType: "commonjs",
+    },
+  },
   pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
+  {
+    rules: {
+      ...tseslint.configs.recommended.rules,
+    },
+  },
 ];
