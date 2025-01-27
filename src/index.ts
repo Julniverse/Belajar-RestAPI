b93ca3d98f782daadab2bf99807dcce116782608
Julniverse <juliandikurniansyah@gmail.com>
2025-01-27 21:35:45 +0700

    Day 6

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
-import express, { Application, Request, Response, NextFunction } from "express";
+import express, { Application } from 'express'
+import { routes } from './routes'
 
-const app: Application = express();
-const port: number = 8080;
+const app: Application = express()
+const port: number = 8080
 
-app.use("/health", (req: Request, res: Response, next: NextFunction) => {
-  res.status(200).send({ status: "200", data: "hellow" });
-});
+routes(app)
 
-app.listen(port, () => console.log("Server is listening on port 8080"));
+app.listen(port, () => console.log('Server is listening on port 8080'))
